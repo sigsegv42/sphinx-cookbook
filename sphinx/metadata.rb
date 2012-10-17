62e8d27714c548932ef821ce928bea460857e8e0
Joshua Farr <joshua.farr@firstgiving.com>
2012-10-17 16:54:54 -0700

    remove long description

--- sphinx/metadata.rb
+++ sphinx/metadata.rb
@@ -2,7 +2,6 @@ maintainer       "Alex Soto"
 maintainer_email "apsoto@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures sphinx search engine."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.5"
 recipe           "sphinx"
 depends "build-essential"
